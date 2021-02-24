cf9ba06d4e41cf7106aeee1d64cca528f8fc6df8
Romeodewit <romeo@classicremedies.co.uk>
2021-02-24 14:57:35 +0000

    added csvconvert

--- model/bank.js
+++ model/bank.js
@@ -1,16 +1,27 @@
-const Papa = require("papaparse");
+const CSVToJSON = require('csvtojson');
 
 // export default class Bank {
 //   constructor(){
 //   }
 // }
-const url = "https://raw.githubusercontent.com/CorndelWithSoftwire/supportbank-js/master/resources/Transactions2014.csv"
 
-Papa.parse(url, {
-  download: false,
-  header: true,
-  delimiter: ",",
-	complete: function(results) {
-		console.log(results);
-	}
+let array = [];
+
+const csvFiles = ["D:\\Code\\training\\Corndell\\day2\\SupportBank\\csv\\transactions.csv"];
+
+async function readCsv(filepath) {
+  const parsedFile = await CSVToJSON().fromFile(filepath)
+    .then(parsedCsv => {
+      parsedCsv.forEach(value => array.push(value))
+    }).catch(err => {
+      console.log(err);
+    });
+  return parsedFile;
+}
+
+const transactions = Promise.resolve(readCsv(csvFiles[0]))
+
+transactions.then(() => {
+  console.log(array.length)
 });
+// transactions(csvFiles[0]);
